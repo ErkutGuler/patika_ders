f7bed108beb8c64e3c992657092ddd654bad718e
Erkut Güler <erkut131@gmail.com>
2023-04-10 14:43:04 +0300

    Mesajı ve Hoşgeldin Getiriyor

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        String inputString; 
+        Console.Write("Mesaj: ");
+        inputString = Console.ReadLine(); 
+        Console.WriteLine("Hello, World.");
+        Console.WriteLine(inputString);
+        }
+    }
+}
